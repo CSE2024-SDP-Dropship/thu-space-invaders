999d2c3ab3d079dd874faa0b5ae6dae3d402f8db
jjangjjh2222 <jjangjjh2222@naver.com>
2022-12-06 22:44:23 +0900

    [ADD] FrameTest.java

---
+++ test/engine/FrameTest.java
@@ -0,0 +1,33 @@
+package engine;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrameTest {
+    int width, height;
+    Frame frame;
+
+    @BeforeEach
+    void setUp() {
+        width = 200;
+        height = 200;
+        frame = new Frame(width, height);
+    }
+
+    @Test
+    void setScreen() {
+        assertNotNull(frame);
+    }
+
+    @Test
+    void getWidth() {
+        assertEquals(width, frame.getWidth());
+    }
+
+    @Test
+    void getHeight() {
+        assertEquals(height-28, frame.getHeight());
+    }
+}
